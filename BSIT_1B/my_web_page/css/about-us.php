b33d91004f8ba8eff79c41620f5bf4d322cac7ff
kayeshavildev <s2501231@usls.edu.ph>
2026-06-27 02:44:49 +0800

    Create about-us.php with university information
    Added content about the University of St. La Salle, including campus structure and academic programs.

---
+++ BSIT_1B/my_web_page/css/about-us.php
@@ -0,0 +1,24 @@
+
+				<h1 align="center">About us</h1>
+				<br>
+                <p> The University of St. La Salle (USLS) in Bacolod City is a leading Catholic educational institution in Negros Occidental </p>
+
+				<h2 align="center">Campus & Structure</h2>
+                <ul>
+                      <li> Main Campus: Located on La Salle Avenue, Bacolod City, featuring a 10-hectare urban campus. </li> 
+                      <li> Integrated School (USLS-IS): Operates across campuses on La Salle Ave., Montelibano Dr., and Alunan. </li> 
+                      <li> Liceo de La Salle: The Senior High School unit, which utilizes a specialized clustering curriculum. </li> 
+                </ul>
+	   		</div>
+                <h2 align="center">Academic Programs</h2>
+            <ol>
+		       
+		       <li> Yu An Log College of Business and Accountancy</li>
+		       <li> College of Engineering</li>
+		       <li> College of Computing Studies </li>
+		       <li> College of Arts and Sciences </li>
+		       <li> College of Nursing </li>
+		       <li> College of Tourism and Hospitality </li>
+		       <li> College of Education </li>
+
+		</ol>
