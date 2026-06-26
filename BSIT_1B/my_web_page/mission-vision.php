22e81854698def029abcf34fd0688f0eb5db581a
kayeshavildev <s2501231@usls.edu.ph>
2026-06-27 02:48:22 +0800

    Create mission-vision.php

---
+++ BSIT_1B/my_web_page/mission-vision.php
@@ -0,0 +1,7 @@
+
+				<h1 align="center">Vision</h1>
+				<br>
+                <p align="center"> The USLS envisions itself as a premier, responsive learning community, advancing human and Christian education through research, innovation, and social engagement in Asia and the Pacific </p>
+				
+                <h2 align="center">Mission</h2>
+				<p align="center"> Rooted in the Catholic tradition and inspired by St. John Baptist de La Salle, the institution aims to form individuals with integrity and excellence, committed to serving humanity and the Earth. </p>
