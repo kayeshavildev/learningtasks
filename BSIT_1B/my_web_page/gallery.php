94d1b18dc363b4b99a8e70bcbedd72ea26a8fb9c
kayeshavildev <s2501231@usls.edu.ph>
2026-06-27 02:50:15 +0800

    Create gallery.php

---
+++ BSIT_1B/my_web_page/gallery.php
@@ -0,0 +1,9 @@
+<h1 align="center">Gallery</h1>
+				<br>
+				<ul>
+                  <img src= "gallery1.jpg" alt="txt" width=250 height=250>
+                  <img src= "gallery2.jpg" alt="txt" width=250 height=250>
+                  <img src= "gallery3.jpg" alt="txt" width=250 height=250>
+                  <img src= "gallery4.jpg" alt="txt" width=250 height=250>
+                </ul>
+    
