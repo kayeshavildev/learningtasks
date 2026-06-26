266b821ea88a1dc889de52b16ac5fe51cbb27dea
kayeshavildev <s2501231@usls.edu.ph>
2026-06-27 02:31:20 +0800

    Add initial HTML structure for Activity 05

---
+++ activity_05/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>..:Activity 05:..</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+        </div>
+    </div>
+</body>
+</html>
