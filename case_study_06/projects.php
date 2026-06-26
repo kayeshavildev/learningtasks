9d42ab7a9abd184f6d185a97ee50054579af60ec
kayeshavildev <s2501231@usls.edu.ph>
2026-06-27 02:56:55 +0800

    Create projects.php

---
+++ case_study_06/projects.php
@@ -0,0 +1,25 @@
+<h1 align='center' style="color: white;">Projects</h1>
+
+<table border="1" cellpadding="5" cellspacing="0" width="75%" align="center">
+    <thead>
+        <tr style="color: darkgray;">
+            <th width="30%">Project</th>
+            <th>Sample Image</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td style="color: white;">Recycling Sessions</td>
+			<td><img src="recycling.jpg" width="670" height="402"></td>
+        </tr>
+        <tr>
+            <td style="color: white;">Basic Computer Tutorials</td>
+			<td><img src="computer.jpg" width="690" height="402"></td>
+
+        </tr>
+        <tr>
+            <td style="color: white;">Coastal Clean-up</td>
+			<td align='center'><img src="clean.jpg" width="600" height="402"></td>
+        </tr>
+    </tbody>
+</table>
