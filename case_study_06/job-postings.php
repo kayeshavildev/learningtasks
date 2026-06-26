0fcc806e78db115d568ac0f18893d65204af57a5
kayeshavildev <s2501231@usls.edu.ph>
2026-06-27 02:59:41 +0800

    Add job postings for various positions

---
+++ case_study_06/job-postings.php
@@ -0,0 +1,28 @@
+<h1 align='center'>Job Postings</h1>
+
+<div>
+    <ul>
+        <li>
+            <b><h3>Frontend Developer</h3></b>
+            <u><b>Company:</b></u> PixelForge Digital <br>
+            <u><b>Location:</b></u> Remote (Philippines) <br>
+            <u><b>Job Type:</b></u> Full-time
+        </li>
+        <br>
+
+        <li>
+            <b><h3>Virtual Assistant</h3></b>
+            <u><b>Company:</b></u> Nova Support Services <br>
+            <u><b>Location:</b></u> Hybrid (Metro Manila) <br>
+            <u><b>Job Type:</b></u> Part-time
+        </li>
+        <br>
+
+        <li>
+            <b><h3>Marketing Coordinator</h3></b>
+            <u><b>Company:</b></u> BrightWave Marketing <br>
+            <u><b>Location:</b></u> On-site (Cebu City) <br>
+            <u><b>Job Type:</b></u> Full-time
+        </li>
+    </ul>
+</div>
