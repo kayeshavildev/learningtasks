162fa92e6ebd510bfd7a17216e2e1afcb00c43af
kayeshavildev <s2501231@usls.edu.ph>
2026-06-27 02:56:07 +0800

    Create home.php with latest news section
    Add home page content with latest news articles.

---
+++ case_study_06/home.php
@@ -0,0 +1,25 @@
+<h1 align='center'>Home</h1>
+<p align='center'>-------------------------------------------------------------</p>
+<h2 align='center'>LATEST NEWS</h2><br><br>
+
+<div>
+<h3><i>Nation Impeachment votes don’t always haunt senators, history shows</i></h3>
+</p>As the Senate prepares for Vice President Sara Duterte’s impeachment trial, an analysis of past proceedings suggests a senator’s vote in a high-stakes impeachment case does not necessarily determine future electoral survival.</p>
+
+<p>MANILA, Philippines — As political tensions build ahead of Vice President Sara Duterte’s impeachment trial, one question continues tosurface in public discussions:
+Will senators who vote one way or another eventually pay a political price? The assumption is familiar in Philippine politics.
+Many believe voters remember defining moments and eventually punish public officials who take unpopular positions during national controversies. </p>
+<img src="senate.jpg" width="600" height="375">
+</div>
+
+<br><br>
+
+<div>
+<h3><i>PH, Germany celebrate 7-decade ties, vow stronger partnership</i></h3>
+<p>MANILA – The Philippines and Germany reaffirmed their commitment to strengthening relations Tuesday as they celebrated over seven decades of diplomatic ties during a state luncheon hosted by President Ferdinand R. Marcos Jr. for German President Frank-Walter Steinmeier.
+
+In his toast remarks delivered at Malacañan Palace in Manila, Marcos underscored the importance of continued friendship, enhanced cooperation and shared prosperity between the two nations, recognizing Germany as a key partner of the Philippines in national development.
+<br><br>
+He welcomed the official German delegation with the country’s trademark hospitality and raised a toast to the continued good health and success of Steinmeier and his spouse, as well as to Filipinos and Germans working together in pursuit of peace.</p>
+<img src="german.jpg" width="600" height="375">
+</div>
