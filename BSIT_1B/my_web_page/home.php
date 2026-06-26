8c80152d589d81eba0d6d67077f3d68711de57da
kayeshavildev <s2501231@usls.edu.ph>
2026-06-27 02:47:18 +0800

    Create home.php

---
+++ BSIT_1B/my_web_page/home.php
@@ -0,0 +1,11 @@
+
+				<h1 align="center">Home</h1>
+				<br>
+                <p><b>Student ID No.:</b> 2501231</p>
+				<p><b>Full Name:</b> Kristine Ayesha F. Villanueva </p>
+				<p><b>Program/Degree:</b> BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY</p>
+				<p><b>Year Level:</b> First Year</p>
+				<p><b>Student Status:</b> Old</p>
+                <p><b>Email:</b> s2501231@usls.edu.ph</p>
+				<p><b>Phone Number:</b> (+63) 123 456 7890</p>
+                <p><b>Present/permanent Address:</b> St. Francis Subd., Phase 7, Silay City </p>
