abb439548fefabf317e2b403e6ee77c7c6fc6d92
kayeshavildev <s2501231@usls.edu.ph>
2026-06-27 02:57:39 +0800

    Add contact us page with contact information

---
+++ case_study_06/contact-us.php
@@ -0,0 +1,16 @@
+<title>Contact Us</title>
+
+<h1 align='center'>Contact Us</h1>
+
+<section>
+    <p>Email: lasalle@usls.edu.ph</p>
+    <p>Phone: +63 923 456 767</p>
+    <p>Address: La Salle Ave., Bacolod City, Philippines</p>
+</section>
+
+<footer>
+    <p>2026 University of St. La Salle</p>
+</footer>
+
+</body>
+</html>
